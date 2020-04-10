f3736c5f3b58d890a30dcab386c6d7c1657a6872
claremboyle <32604195+claremboyle@users.noreply.github.com>
2020-04-10 14:16:22 -0700

    Added comments

--- FirstDetection.py
+++ FirstDetection.py
@@ -6,10 +6,11 @@ execution_path = os.getcwd()
 detector = ObjectDetection()    # DEFINE object detection variable
 detector.setModelTypeAsRetinaNet()
 detector.setModelPath( os.path.join(execution_path , "resnet50_coco_best_v2.0.1.h5"))
+    ## !!! 'resnet50_coco_best_v2.0.1.h5' is the RetinaNet model file used for object detection
 detector.loadModel()
 
 detections = detector.detectObjectsFromImage(input_image=os.path.join(execution_path , "image.jpg"), output_image_path=os.path.join(execution_path , "imagenew.jpg"))
-## !!! IMAGE FILE MUST BE CALLED 'image.jpg'
+    ## !!! IMAGE FILE MUST BE CALLED 'image.jpg'
 
 for eachObject in detections:
     print(eachObject["name"] , " : " , eachObject["percentage_probability"] )
